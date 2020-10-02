f73d803603201f2256d0e22396828bbe08440659
Ilya Kolb <kolbi@hhmi.org>
2020-10-02 15:02:27 -0400

    load and record actual video

--- make_cellattached_video.py
+++ make_cellattached_video.py
@@ -6,6 +6,10 @@ Created on Sun Sep 27 21:50:21 2020
 
 real-time frame grabbing
 
+INSTRUCTIONS
+    Need to have ImageMagick Installed
+    Run this with suite2p_backup conda environment (due to pyWavesurfer compatibility issues)
+@todo: online_ephys_analysis should make dff and spike struct
 """
 
 import numpy as np
@@ -13,40 +17,83 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import os
 import tifffile as tif
-
-def make_square_axes(ax):
-    """Make an axes square in screen units.
-
-    Should be called after plotting.
-    """
-    ax.set_aspect(1 / ax.get_data_ratio())
-
+import warnings
+from utils_ephys import extract_tuning_curve
 plt.close('all')
+
+start_frame = 500 # start at this frame index
+n_frames = 50 # num frames to write
+write_movie_fps = 30 # fps of resulting movie (SHOULD = MOVIE FRAMERATE?)
+
+vis_stim_file = r'Z:\rozsam\raw\visual_stim\20200831-anm479116\Cell3_Run03\31-Aug-2020_11_51_11_03.mat'
+h5_file = r'Z:\rozsam\raw\ephys\20200831-anm479116\Cell3\cell3_stim03_0001.h5'
+
+# load dff_and_spike struct in the form 'Z:\rozsam\suite2p\20200831-anm479116\Cell3\cell3_stim03_00001\plane0'
+movie_dir = r'Z:\rozsam\suite2p\20200831-anm479116\Cell3\cell3_stim03_00001\plane0'
+movie_write_dir = 'F:/jgcamp8_movies/test.mp4'
+
+dff_file = os.path.join(movie_dir, 'dFF.npy')
+
+ephys_out = extract_tuning_curve(h5_file, vis_stim_file)
+ephys_t_s = ephys_out['ephys_t']
+ophys_t_s = ephys_out['frame_times']
+dFF = np.load(dff_file)
+frame_sRate =  1/np.mean(np.diff(ophys_t_s))
+end_frame = start_frame + n_frames
+start_s, end_s = np.array([start_frame, end_frame])/frame_sRate
+
+
+# combined and cropped tif file of cell-attached recording
+# stream = tif.imread(os.path.join(movie_dir,'reg_tif', 'combo.tif'))
 stream = tif.imread(r'F:\jgcamp8_movies\sample\combined.tif')
 
 # remove z axis (artifact of fiji import)
 stream = stream.reshape((stream.shape[0]*stream.shape[1], stream.shape[2],stream.shape[3]))
 
-movie_write_dir = r'F:\jgcamp8_movies\sample\test.mp4'
-n_frames = 1000 # num frames to write
-# First set up the figure, the axis, and the plot element we want to animate
+if stream.shape[1] != stream.shape[2]:
+    warnings.warn("WARNING: stream image is not square!")
+
+
 fig = plt.figure()
 ax1,ax2 = fig.subplots(1,2)
 ax1.imshow(stream[0,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
 ax1.axis("off")
 
 line, = ax2.plot([], [], lw=2)
-ax2.set_xlim(0,2)
-ax2.set_ylim(-2,2)
-ax2.set_aspect(1/2)
-# ax2.axis("square")
-# ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
 
 
-# plt.tight_layout()
+x_lim = (-2,2)
+y_lim = (-1,3)  # @todo: determine automatically?
+
+ax2.set_xlim(x_lim)
+ax2.set_ylim(y_lim)
+ax2.set_aspect((x_lim[1]-x_lim[0]) / (y_lim[1]-y_lim[0])) # make sure plot is square, should be xlim / ylim
+
+FFwriter = animation.FFMpegWriter(fps=write_movie_fps, extra_args=['-vcodec', 'libx264'])
+
+frame_iter = range(start_frame, end_frame)
+
+# set ImageMagick directory
+ff_path = os.path.join('C:/', 'ImageMagick-7.0.10-Q16-HDRI', 'ffmpeg.exe')
+plt.rcParams['animation.ffmpeg_path'] = ff_path
+
+# always playing movie @30 FPS
+# @todo: may need to address case where write_movie_fps != recording fps
+indices_to_shift = np.where(ephys_t_s <= 1/write_movie_fps,1,0).sum() # how many indices to shift every movie frame
+with FFwriter.saving(fig, movie_write_dir, n_frames):
+    for i, frame_i in enumerate(frame_iter):
+        ax1.imshow(stream[frame_i,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
+        
+        x = ophys_t_s - ophys_t_s[start_frame] - i
+        y = np.roll(dFF, i)
+        line.set_data(x, y)
+        FFwriter.grab_frame()
+        print("done grabbing frame {}".format(i))
 plt.show()
 
 
+# mock data
+'''
 FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
 with FFwriter.saving(fig, movie_write_dir, n_frames):
     for i in range(n_frames):
@@ -55,38 +102,4 @@ with FFwriter.saving(fig, movie_write_dir, n_frames):
         y = np.sin(2 * np.pi * (x - 0.01 * i))
         line.set_data(x, y)
         FFwriter.grab_frame()
-
-
 '''
-def init():
-    line.set_data([], [])
-    return line,
-
-# animation function.  This is called sequentially
-'''
-'''
-def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-'''
-# call the animator.  blit=True means only re-draw the parts that have changed.
-# anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
-
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
-
-# need these
-ff_path = os.path.join('C:/', 'ImageMagick-7.0.10-Q16-HDRI', 'ffmpeg.exe')
-plt.rcParams['animation.ffmpeg_path'] = ff_path
-
-# FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-# anim.save('basic_animation.mp4', writer = FFwriter)
-# s = anim.to_html5_video()
-# with open("myvideo.html", "w") as f:
-#     print(anim.to_html5_video(), file=f)
-# 
---
+++ utils.py
@@ -0,0 +1,199 @@
+''' 
+copied from https://github.com/ilyakolb/2P_cellAttached_pipeline
+'''
+
+from scipy import signal
+from scipy.io import loadmat
+import scipy.ndimage as ndimage
+import numpy as np
+
+
+
+def rollingfun(y, window = 10, func = 'mean'):
+    """
+    rollingfun
+        rolling average, min, max or std
+    
+    @input:
+        y = array, window, function (mean,min,max,std)
+    """
+    
+    y = np.concatenate([y[window::-1],y,y[:-1*window:-1]])
+    ys = list()
+    for idx in range(window):    
+        ys.append(np.roll(y,idx-round(window/2)))
+    if func =='mean':
+        out = np.nanmean(ys,0)[window:-window]
+    elif func == 'min':
+        out = np.nanmin(ys,0)[window:-window]
+    elif func == 'max':
+        out = np.nanmax(ys,0)[window:-window]
+    elif func == 'std':
+        out = np.nanstd(ys,0)[window:-window]
+    elif func == 'median':
+        out = np.nanmedian(ys,0)[window:-window]
+    else:
+        print('undefinied funcion in rollinfun')
+    return out
+
+
+
+def norm0To1(x):
+    """
+    normalizes x to 0 to 1 range
+    """
+    return (x-min(x)) / (max(x) - min(x))
+ 
+
+
+def hpFilter(sig, HPfreq, order, sRate, padding = True):
+    """
+    High pass filter
+    -enable padding to avoid edge artefact
+    """
+    padlength=10000
+    sos = signal.butter(order, HPfreq, 'hp', fs=sRate, output='sos')
+    if padding: 
+        sig_out = signal.sosfilt(sos, np.concatenate([sig[padlength-1::-1],sig,sig[:-padlength-1:-1]]))
+        sig_out = sig_out[padlength:-padlength]
+    else:
+        sig_out = signal.sosfilt(sos, sig)
+    return sig_out
+
+def gaussFilter(sig,sRate,sigma = .00005):
+    si = 1/sRate
+    #sigma = .00005
+    sig_f = ndimage.gaussian_filter(sig,sigma/si)
+    return sig_f
+    
+
+def findAPs(v, sRate, SN_min = 10,refracter_period = 1,method = 'diff'):
+    """
+    findAPs: identify APs in voltage trace
+    @inputs:
+            v: voltage trace (should already be filtered)
+            sRate: sampling rate
+            SN_min: minimum SNR to be involved as a spike
+    
+    @outputs:
+            peak indices
+            peak SNR
+    """
+    #%%
+    if method == 'diff':
+        v_orig = v.copy()
+        v=np.diff(v)*sRate
+        window = int(sRate*.1)
+        step=int(window/2)
+        starts = np.arange(0,len(v)-window,step)
+        stds = list()
+        for start in starts:
+            stds.append(np.std(v[start:start+window]))
+        stds_roll = rollingfun(stds,100,'min')
+        stds_roll = rollingfun(stds_roll,500,'mean')
+        #%
+        v_scaled = np.copy(v)
+        noise_level = np.ones(len(v)+1)
+        for start,std in zip(starts,stds_roll):
+            v_scaled[start:start+window]=v[start:start+window]/std
+            noise_level[start:start+window]=std
+        v_scaled[start:]=v[start:]/std
+        noise_level[start:]=std
+        peaks_, properties = signal.find_peaks(v_scaled,height = SN_min, distance=int((refracter_period/1000)*sRate))
+        snr = properties['peak_heights']
+        # ignore suspiciuously low or high 'spikes'
+        if len(peaks_)>10:
+            snr_rolling = rollingfun(snr, window = 5, func = 'median')
+            needed_spikes = (snr>snr_rolling*.1) & (snr<snr_rolling*10)
+            peaks_=peaks_[needed_spikes]
+            snr =snr[needed_spikes]
+        peaks_v = list()
+        for peak_dv in peaks_:
+            peaks_v.append(peak_dv+np.argmax(v_orig[peak_dv:int(.001*sRate)+peak_dv]))
+    else:
+        window = int(sRate*10)
+        step=int(window/2)
+        starts = np.arange(0,len(v)-window,step)
+        stds = list()
+        for start in starts:
+            stds.append(np.median(np.abs((v[start:start+window]))/.6745))
+        #%
+        v_scaled = np.copy(v)
+        noise_level = np.ones(len(v))
+        for start,std in zip(starts,stds):
+            v_scaled[start:start+window]=v[start:start+window]/std
+            noise_level[start:start+window]=std
+        v_scaled[start:]=v[start:]/std
+        noise_level[start:]=std
+        peaks_v, properties = signal.find_peaks(v_scaled,height = SN_min, distance=int((refracter_period/1000)*sRate))
+        snr = properties['peak_heights']
+        
+    #%%
+    return peaks_v, snr, noise_level
+    
+
+
+def getEdges(sig, minDist_samples = 50, diffThresh = 1, edge = 'positive'):
+    """
+    getEdges: returns indices of rising/falling edges, for parsing frame fire signal
+    
+    @inputs:
+        sig: [Nx1] numpy array from which to find edges
+        minDist_samples: minimum distance between found edges (in samples)
+        diffThresh: threshold for detecting edge
+        edge: 'positive'
+    @outputs:
+        indices of edges
+    """
+    # take diff of signal and threshold it
+    sig_diff = (np.diff(sig) > diffThresh) if edge == 'positive' else (np.diff(sig) < -1*diffThresh)
+    
+    # find peaks in diff array to get indices
+    peaks, _ = signal.find_peaks(sig_diff, height=diffThresh/2, distance=minDist_samples)
+    
+    # sometimes first frame is rising edge, doesn't get detected by peak finder
+    # if so, insert 0th frame into array
+    if sig_diff[0] > diffThresh:
+        peaks = np.insert(peaks, 0, 0)
+    
+    return peaks
+
+
+def processVisMat(vis_mat_path):
+    """
+    processVisMat
+        load visual stim file and get relevant parameters
+    
+    @input:
+        vis_mat_path: path to vis stim mat file
+    
+    @outputs:
+        vis
+            ncondition: 8
+            ntrial: 5
+            fs: 122
+            stim_dur: 2
+            gray_dur: 2
+            total_frames: 19520
+            total_time: 160
+            stim_init: [1×40 double]
+            numStim: 40
+            angle: [1×40 double]
+            trials: {1×40 cell}
+    """
+    mat = loadmat(vis_mat_path)['vis'][0][0]
+    
+    vis = {}
+    vis['ncondition'] = mat[0][0][0]
+    vis['ntrial'] = mat[1][0][0]
+    vis['fs'] = mat[2][0][0]
+    vis['stim_dur'] = mat[3][0][0]
+    vis['gray_dur'] = mat[4][0][0]
+    vis['total_frames'] = mat[5][0][0]
+    vis['total_time'] = mat[6][0][0]
+    vis['stim_init'] = mat[7][0]
+    vis['numStim'] = mat[8][0][0]
+    vis['angle'] = mat[9][0]
+    vis['trials'] = mat[10]
+    
+    return vis
