2863f9574c16460ece09c310e1b625a05ab55f61
Ilya Kolb <kolbi@hhmi.org>
2020-10-04 17:32:35 -0400

    vertical line to plot

--- make_cellattached_video.py
+++ make_cellattached_video.py
@@ -9,7 +9,12 @@ real-time frame grabbing
 INSTRUCTIONS
     Need to have ImageMagick Installed
     Run this with suite2p_backup conda environment (due to pyWavesurfer compatibility issues)
-@todo: online_ephys_analysis should make dff and spike struct
+
+@todo:
+    - add spike trace
+    - enable movie slow-down or speed-up
+    - aesthetics
+    - vertical line at 0
 """
 
 import numpy as np
@@ -23,7 +28,7 @@ plt.close('all')
 
 start_frame = 500 # start at this frame index
 dpi = 50
-n_frames = 300 # num frames to write
+n_frames = 50 # num frames to write
 write_movie_fps = 10 # fps of resulting movie (SHOULD = MOVIE FRAMERATE?)
 
 x_lim = (-1,1)
@@ -33,7 +38,6 @@ y_lim = (-1,3)  # @todo: determine automatically?
 vis_stim_file = r'Z:\rozsam\raw\visual_stim\20200831-anm479116\Cell3_Run03\31-Aug-2020_11_51_11_03.mat'
 h5_file = r'Z:\rozsam\raw\ephys\20200831-anm479116\Cell3\cell3_stim03_0001.h5'
 
-# load dff_and_spike struct in the form 'Z:\rozsam\suite2p\20200831-anm479116\Cell3\cell3_stim03_00001\plane0'
 movie_dir = r'Z:\rozsam\suite2p\20200831-anm479116\Cell3\cell3_stim03_00001\plane0'
 movie_write_dir = 'F:/jgcamp8_movies/test.mp4'
 
@@ -50,8 +54,8 @@ start_s, end_s = np.array([start_frame, end_frame])/ophys_sRate
 
 
 # combined and cropped tif file of cell-attached recording
-# stream = tif.imread(os.path.join(movie_dir,'reg_tif', 'combo.tif'))
-stream = tif.imread(r'F:\jgcamp8_movies\sample\combined.tif')
+stream = tif.imread(os.path.join(movie_dir,'reg_tif', 'combo_square.tif'))
+# stream = tif.imread(r'F:\jgcamp8_movies\sample\combined.tif')
 
 # remove z axis (artifact of fiji import)
 stream = stream.reshape((stream.shape[0]*stream.shape[1], stream.shape[2],stream.shape[3]))
@@ -65,9 +69,11 @@ ax1,ax2 = fig.subplots(1,2)
 ax1.imshow(stream[0,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
 ax1.axis("off")
 
-line, = ax2.plot([], [], lw=2)
-
-
+line, = ax2.plot([], [], 'k-', lw=2)
+ax2.plot([0, 0], y_lim, 'r--', lw=1) # vertical red line
+ax2.spines["top"].set_visible(False)
+ax2.spines["right"].set_visible(False)
+ax2.spines["left"].set_visible(False)
 ax2.set_xlim(x_lim)
 ax2.set_ylim(y_lim)
 ax2.set_aspect((x_lim[1]-x_lim[0]) / (y_lim[1]-y_lim[0])) # make sure plot is square, should be xlim / ylim
@@ -83,27 +89,16 @@ plt.rcParams['animation.ffmpeg_path'] = ff_path
 # always playing movie @30 FPS
 # @todo: may need to address case where write_movie_fps != recording fps
 
-indices_to_shift = np.where(ephys_t_s <= 1/ophys_sRate,1,0).sum() # how many indices to shift every movie frame
+# indices_to_shift = np.where(ephys_t_s <= 1/ophys_sRate,1,0).sum() # how many indices to shift every movie frame
+
 with FFwriter.saving(fig, movie_write_dir, dpi):
     for i, frame_i in enumerate(frame_iter):
         ax1.imshow(stream[frame_i,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
         
-        x = ophys_t_s - ophys_t_s[start_frame]  # - i/ophys_sRate
+        x = ophys_t_s - ophys_t_s[start_frame]
         y = np.roll(dFF, -1*i)
         line.set_data(x, y)
         FFwriter.grab_frame()
         print("done grabbing frame {}".format(i))
-plt.show()
-
-
-# mock data
-'''
-FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-with FFwriter.saving(fig, movie_write_dir, n_frames):
-    for i in range(n_frames):
-        ax1.imshow(stream[i,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
-        x = np.linspace(0, 2, 1000)
-        y = np.sin(2 * np.pi * (x - 0.01 * i))
-        line.set_data(x, y)
-        FFwriter.grab_frame()
-'''
+    print('DONE')
+# plt.show()
