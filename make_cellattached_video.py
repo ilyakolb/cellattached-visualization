f1484e9de5c6831b04b31be2842995fc53279e2c
Ilya Kolb <kolbi@hhmi.org>
2020-10-04 21:48:09 -0400

    add spike trace, movie speedup, time counter

--- make_cellattached_video.py
+++ make_cellattached_video.py
@@ -14,7 +14,7 @@ INSTRUCTIONS
     - add spike trace
     - enable movie slow-down or speed-up
     - aesthetics
-    - vertical line at 0
+    - time counter
 """
 
 import numpy as np
@@ -26,11 +26,12 @@ import warnings
 from utils_ephys import extract_tuning_curve
 plt.close('all')
 
-start_frame = 500 # start at this frame index
+start_frame = 550 # start at this frame index
 dpi = 50
-n_frames = 50 # num frames to write
+n_frames = 500 # num frames to write
 write_movie_fps = 10 # fps of resulting movie (SHOULD = MOVIE FRAMERATE?)
 
+speed = 2 # (int) set to 1 for regular speed, >1 for faster
 x_lim = (-1,1)
 y_lim = (-1,3)  # @todo: determine automatically?
 
@@ -46,6 +47,7 @@ dff_file = os.path.join(movie_dir, 'dFF.npy')
 ephys_out = extract_tuning_curve(h5_file, vis_stim_file)
 ephys_t_s = ephys_out['ephys_t']
 ophys_t_s = ephys_out['frame_times']
+ap_idx = ephys_out['ap_idx']
 dFF = np.load(dff_file)
 ophys_sRate =  1/np.mean(np.diff(ophys_t_s))
 ephys_sRate = 1/np.mean(np.diff(ephys_t_s))
@@ -60,6 +62,9 @@ stream = tif.imread(os.path.join(movie_dir,'reg_tif', 'combo_square.tif'))
 # remove z axis (artifact of fiji import)
 stream = stream.reshape((stream.shape[0]*stream.shape[1], stream.shape[2],stream.shape[3]))
 
+# downsample by 'speed' if needed
+stream = stream[::speed,:,:]
+
 if stream.shape[1] != stream.shape[2]:
     warnings.warn("WARNING: stream image is not square!")
 
@@ -69,7 +74,10 @@ ax1,ax2 = fig.subplots(1,2)
 ax1.imshow(stream[0,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
 ax1.axis("off")
 
-line, = ax2.plot([], [], 'k-', lw=2)
+line, = ax2.plot(ophys_t_s - ophys_t_s[start_frame], dFF, 'k-', lw=2)
+ap_ticks, = ax2.plot(ephys_t_s[ap_idx],np.ones(len(ap_idx))*np.max(dFF)*-0.1,'r|')
+stopwatch = ax2.text(0.7,0.9,'t = 0 s',ha='left', va='top', weight='bold', size=16, transform=ax2.transAxes)
+
 ax2.plot([0, 0], y_lim, 'r--', lw=1) # vertical red line
 ax2.spines["top"].set_visible(False)
 ax2.spines["right"].set_visible(False)
@@ -77,7 +85,8 @@ ax2.spines["left"].set_visible(False)
 ax2.set_xlim(x_lim)
 ax2.set_ylim(y_lim)
 ax2.set_aspect((x_lim[1]-x_lim[0]) / (y_lim[1]-y_lim[0])) # make sure plot is square, should be xlim / ylim
-
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel(r"$Delta$F/F")
 FFwriter = animation.FFMpegWriter(fps=write_movie_fps, extra_args=['-vcodec', 'libx264'])
 
 frame_iter = range(start_frame, end_frame)
@@ -95,9 +104,15 @@ with FFwriter.saving(fig, movie_write_dir, dpi):
     for i, frame_i in enumerate(frame_iter):
         ax1.imshow(stream[frame_i,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
         
-        x = ophys_t_s - ophys_t_s[start_frame]
-        y = np.roll(dFF, -1*i)
-        line.set_data(x, y)
+        # plot ophys trace
+        line.set_ydata(np.roll(dFF, -1*i*speed))
+        
+        # plot AP markers
+        ap_ticks.set_xdata(ephys_t_s[ap_idx] - ophys_t_s[start_frame] - i*speed/ophys_sRate)
+        
+        # update stopwatch
+        stopwatch.set_text('t= {} s'.format(round(i*speed/ophys_sRate, 2)))
+        
         FFwriter.grab_frame()
         print("done grabbing frame {}".format(i))
     print('DONE')
