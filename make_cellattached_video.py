52e9bf79976e3fa7b6d98f19ae085ad11a59fa77
Ilya Kolb <kolbi@hhmi.org>
2020-10-06 09:28:54 -0400

    dark mode option

--- make_cellattached_video.py
+++ make_cellattached_video.py
@@ -10,10 +10,6 @@ INSTRUCTIONS
     Need to have ImageMagick Installed
     Run this with suite2p_backup conda environment (due to pyWavesurfer compatibility issues)
 
-@todo:
-    - aesthetics
-    - auto adjust ylim
-    - fix jerkiness
 """
 
 import numpy as np
@@ -25,14 +21,17 @@ import warnings
 from utils_ephys import extract_tuning_curve
 plt.close('all')
 
+invert_colors = True # True for white on black background
 start_frame = 2450 # start at this frame index
 dpi = 100
-n_frames = 1000 # num frames to write
+n_frames = 100 # num frames to write
 write_movie_fps = 30 # fps of resulting movie
 
-speed = 1 # (int) set to 1 for regular speed, >1 for faster
+speed = 1 # (int) set to 1 for regular speed, >1 for faster. speed > 1 results in jitteriness
 x_lim = (-1,1)
 
+if invert_colors:
+    plt.style.use('dark_background')
 
 vis_stim_file = r'Z:\rozsam\raw\visual_stim\20200831-anm479116\Cell3_Run03\31-Aug-2020_11_51_11_03.mat'
 h5_file = r'Z:\rozsam\raw\ephys\20200831-anm479116\Cell3\cell3_stim03_0001.h5'
@@ -68,17 +67,19 @@ if stream.shape[1] != stream.shape[2]:
 
 
 fig = plt.figure(figsize=[6,3])
+# figure color
+# fig.patch.set_facecolor('black' if invert_colors else 'white')
 ax1,ax2 = fig.subplots(1,2)
 ax1.imshow(stream[0,:,:], extent=[0,100,0,1], aspect=100, cmap='gray')
 ax1.axis("off")
 
-line, = ax2.plot(ophys_t_s - ophys_t_s[start_frame], dFF, 'k-', lw=2)
+line, = ax2.plot(ophys_t_s - ophys_t_s[start_frame], dFF, 'w-' if invert_colors else 'k-', lw=2)
 ap_ticks, = ax2.plot(ephys_t_s[ap_idx],np.ones(len(ap_idx))*np.max(dFF)*-0.1,'r|')
 stopwatch = ax2.text(0.65,0.9,'t = 0 s',ha='left', va='top', weight='bold', size=14, transform=ax2.transAxes)
 
 y_lim = (-1,dFF.max()+0.1)
 
-ax2.plot([0, 0], y_lim, 'r--', lw=1) # vertical red line
+ax2.plot([0, 0], y_lim, 'r--', lw=1.5) # vertical red line
 ax2.spines["top"].set_visible(False)
 ax2.spines["right"].set_visible(False)
 ax2.spines["left"].set_visible(False)
